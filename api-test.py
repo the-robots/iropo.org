cdc5ff0be03a2902490001901e62f6f249d07776
mx1010 <melscoop@icloud.com>
2023-01-01 21:05:48 -0500

    adding code to list column names

--- api-test.py
+++ api-test.py
@@ -5,6 +5,12 @@ import requests
 # Read the Excel file into a DataFrame
 df = pd.read_excel('localAndStateAgencies-NC.xlsx')
 
+# Get a list of all the column names in the DataFrame
+column_names = df.columns.tolist()
+
+# Extract the second column from the DataFrame as a list (assuming the ORI column is the second column)
+identifiers = df[column_names[1]].tolist()
+
 # Extract the ORI column from the DataFrame as a list
 identifiers = df['ORI'].tolist()
 
