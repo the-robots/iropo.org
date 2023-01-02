0678fa5fa877f263a3f98f03a5b24b7ad04467ea
mx1010 <melscoop@icloud.com>
2023-01-01 20:56:34 -0500

    Update api-test.py

--- api-test.py
+++ api-test.py
@@ -11,19 +11,6 @@ identifiers = df['ORI'].tolist()
 # Set the base URL for the API endpoint
 api_endpoint = "https://api.usa.gov/crime/fbi/sapi/api/data/nibrs/animal-cruelty/offender/agencies/{}/count?API_KEY=46pdC8k9DkXmNtyAMPHobBPCZ2cXHgs0grLnRc31"
 
-# Initialize an empty string to hold the HTML for the table rows# Import the necessary libraries
-import pandas as pd
-import requests
-
-# Read the Excel file into a DataFrame
-df = pd.read_excel('localAndStateAgencies-NC.xlsx')
-
-# Extract the ORI column from the DataFrame as a list
-identifiers = df['ORI'].tolist()
-
-# Set the base URL for the API endpoint
-api_endpoint = "https://api.usa.gov/crime/fbi/sapi/api/data/nibrs/animal-cruelty/offender/agencies/{}/count?API_KEY=46pdC8k9DkXmNtyAMPHobBPCZ2cXHgs0grLnRc31"
-
 # Initialize an empty string to hold the HTML for the table rows
 rows = ''
 
@@ -43,23 +30,14 @@ for identifier in identifiers:
     animal_cruelty_offenders = data['animal_cruelty_offenders']
 
     # Use the extracted data to build the HTML for a table row
-rows = ''
+    row = f"<tr><td>{agency_name}</td><td>{animal_cruelty_offenders}</td></tr>"
 
-# Iterate through the list of identifiers
-for identifier in identifiers:
-  # Construct the full API endpoint URL
-  url = api_endpoint.format(identifier)
+    # Append the row HTML to the rows string
+    rows += row
+  else:
+    # If the request was not successful, print an error message
+    print(f"Error: {response.status_code}")
 
-  # Send a GET request to the API endpoint
-  response = requests.get(url)
-
-  # Check if the request was successful
-  if response.status_code == 200:
-    # Extract the data from the response object
-    data = response.json()
-    agency_name = data['agency_name']
-    animal_cruelty_offenders = data['animal_cruelty_offenders']
-
-    # Use the extracted data to build the HTML for a table row
-    row = f"<tr><td>{agency_name}</td
-
+# Use JavaScript or a library like jQuery to append the rows to the table on the web page
+# Assume that the table body has an ID of "table-body"
+document.getElementById("table-body").innerHTML = rows
